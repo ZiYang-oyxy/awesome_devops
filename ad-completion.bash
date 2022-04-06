ea8e5e2ac48e4669f42fc6d854f1f141c7eddd16
ziyang <hh123okbb@gmail.com>
2022-04-06 09:50:57 +0800

    completion: autocomplete filename

--- ad-completion.bash
+++ ad-completion.bash
@@ -1,15 +1,37 @@
 #!/bin/bash
 # refer to https://github.com/iridakos/bash-completion-tutorial
 
+_mycmd_compgen_filenames() {
+    local cur="$1"
+
+    # Files, excluding directories:
+    grep -v -F -f <(compgen -d -P ^ -S '$' -- "$cur") \
+        <(compgen -f -P ^ -S '$' -- "$cur") |
+        sed -e 's/^\^//' -e 's/\$$/ /'
+
+    # Directories:
+    compgen -d -S / -- "$cur"
+}
+
 _ad()
 {
-    if [ "${#COMP_WORDS[@]}" != "2" ]; then
-        return
-    fi
-
-    tools=`ls ~/.awesome_devops/tools 2> /dev/null`
-    ext_tools=`ls ~/.awesome_devops/ext_tools 2>/dev/null`
-    COMPREPLY=($(compgen -W "help upgrade tree put tput get dput dget deploy $tools $ext_tools" "${COMP_WORDS[1]}"))
+    case "$COMP_CWORD" in
+        1)
+            tools=`ls ~/.awesome_devops/tools 2> /dev/null`
+            ext_tools=`ls ~/.awesome_devops/ext_tools 2>/dev/null`
+            COMPREPLY=($(compgen -W "help upgrade tree put tput get dput dget deploy $tools $ext_tools" "${COMP_WORDS[1]}"))
+            ;;
+        2)
+            case ${COMP_WORDS[1]} in
+                *)
+                    COMPREPLY=($(_mycmd_compgen_filenames "${COMP_WORDS[2]}"))
+                    ;;
+            esac
+            ;;
+        *)
+            return
+            ;;
+    esac
 }
 
 complete -F _ad ad
