c0cde539cfdd2a71a26ff72649d8d46179315ec6
ziyang <hh123okbb@gmail.com>
2026-02-04 21:29:43 +0800

    update

--- plugins/oyxy_dotfiles/.config/nvim/lua/config/snacks.lua
+++ plugins/oyxy_dotfiles/.config/nvim/lua/config/snacks.lua
@@ -4,17 +4,6 @@ return {
   picker = {
     sources = {
       explorer = {
-        config = function(opts)
-          if opts.cwd == nil or opts.cwd == "" then
-            local bufname = vim.api.nvim_buf_get_name(0)
-            if bufname == "" then
-              opts.cwd = vim.loop.cwd()
-            else
-              opts.cwd = vim.fn.fnamemodify(bufname, ":p:h")
-            end
-          end
-          return opts
-        end,
         layout = { layout = { position = "right" } },
         win = {
           list = {
