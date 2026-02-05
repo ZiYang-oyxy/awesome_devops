3456098e2976924911b1df4ffbe0b4a9382ad658
ziyang <hh123okbb@gmail.com>
2026-02-05 19:44:46 +0800

    更新快捷键

--- plugins/oyxy_dotfiles/.config/nvim/lua/config/keymaps.lua
+++ plugins/oyxy_dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -5,8 +5,18 @@
 local keymap = vim.keymap.set
 
 keymap("n", "<leader>q", "<cmd>qa!<cr>", { desc = "Quit all" })
+keymap("n", "<leader>a", "A", { desc = "Append end of line" })
+-- keymap("n", "<leader>y", "+y", { desc = "Yank to clipboard" })
+-- keymap("n", "<leader>p", "+p", { desc = "Paste from clipboard" })
+keymap("x", "p", '"_dP') -- 使用黑洞寄存器，避免覆盖clipboard
+keymap({ "n", "v" }, "d", '"_d') -- 使用黑洞寄存器clipboard
 keymap("n", "q", "<Nop>", { desc = "Disable macro recording" })
 keymap("n", "Q", "<Nop>", { desc = "Disable Ex mode" })
+keymap("n", "-", "^")
+keymap("n", "=", "$")
+keymap("v", "-", "^")
+keymap("v", "=", "$")
+keymap({ "n", "v" }, "\\", "%", { desc = "Jump to match" })
 keymap("n", "<leader>ss", "<cmd>source $MYVIMRC<cr>", { desc = "Reload config" })
 keymap("n", "<leader>ee", "<cmd>edit ~/Documents/byte/Notes/note.md<cr>", { desc = "Edit note.md" })
 keymap("n", "<F9>", function()
@@ -15,8 +25,7 @@ keymap("n", "<F9>", function()
     warn_no_results = false,
     open_no_results = true,
     win = {
-      position = "left",
-      size = math.min(60, math.floor(vim.o.columns * 0.3)),
+      position = "bottom",
     },
   })
 end, { desc = "Trouble LSP (toggle)" })
