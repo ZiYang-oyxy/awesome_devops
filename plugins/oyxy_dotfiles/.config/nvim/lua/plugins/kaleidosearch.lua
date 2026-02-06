6d1e38f827f6d5ae2d1dd980bfd7b7b9e50635b5
ziyang <hh123okbb@gmail.com>
2026-02-07 02:56:06 +0800

    mark_cx

--- plugins/oyxy_dotfiles/.config/nvim/lua/plugins/kaleidosearch.lua
+++ plugins/oyxy_dotfiles/.config/nvim/lua/plugins/kaleidosearch.lua
@@ -1,6 +1,7 @@
 return {
   {
     "hamidi-dev/kaleidosearch.nvim",
+    enabled = false,
     config = function()
       require("config.kaleidosearch").setup({
         regex_mode = true,
---
+++ plugins/oyxy_dotfiles/.config/nvim/lua/plugins/mark.lua
@@ -0,0 +1,83 @@
+return {
+  {
+    dir = "/Volumes/code/vibecoding/mark/vim-mark_cx",
+    name = "mark.nvim",
+    main = "mark",
+    lazy = false,
+    opts = {
+      keymaps = { preset = "none" },
+      auto_save = true,
+      auto_load = false,
+      ui = {
+        enhanced_picker = false,
+        float_list = true,
+      },
+    },
+    keys = {
+      {
+        "!",
+        function()
+          require("mark").mark_word_or_selection({ group = vim.v.count })
+        end,
+        mode = { "n", "x" },
+        desc = "Mark: Toggle word or selection",
+        silent = true,
+      },
+      {
+        "<leader><cr>",
+        function()
+          require("mark").clear_all()
+        end,
+        mode = "n",
+        desc = "Mark: Clear all",
+        silent = true,
+      },
+      {
+        "n",
+        function()
+          require("mark").search_any_mark(false, vim.v.count1)
+        end,
+        mode = "n",
+        desc = "Mark: Next any match",
+        silent = true,
+      },
+      {
+        "N",
+        function()
+          require("mark").search_any_mark(true, vim.v.count1)
+        end,
+        mode = "n",
+        desc = "Mark: Prev any match",
+        silent = true,
+      },
+      {
+        "#",
+        function()
+          require("mark").search_current_mark(false, vim.v.count1)
+        end,
+        mode = "n",
+        desc = "Mark: Next current match",
+        silent = true,
+      },
+      {
+        "@",
+        function()
+          require("mark").search_current_mark(true, vim.v.count1)
+        end,
+        mode = "n",
+        desc = "Mark: Prev current match",
+        silent = true,
+      },
+      {
+        "<leader>`",
+        function()
+          require("mark").list()
+        end,
+        mode = "n",
+        desc = "Mark: List all",
+        silent = true,
+        nowait = true,
+      },
+    },
+  },
+}
