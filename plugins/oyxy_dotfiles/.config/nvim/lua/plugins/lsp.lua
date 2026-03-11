ae0a960e86a6d29ba67f4999036f189961187208
ziyang <hh123okbb@gmail.com>
2026-03-11 23:28:48 +0800

    feat(nvim): improve LSP search mappings

--- plugins/oyxy_dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ plugins/oyxy_dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -17,6 +17,27 @@ local function ivy_opts(opts)
   }, opts or {})
 end
 
+local function sanitize_search_text(text)
+  if not text then
+    return nil
+  end
+  local sanitized = text:gsub("\r", " "):gsub("\n", " "):gsub("^%s+", ""):gsub("%s+$", "")
+  if sanitized == "" then
+    return nil
+  end
+  return sanitized
+end
+
+local function get_visual_selection_text_by_yank()
+  local reg_name = "z"
+  local reg_value = vim.fn.getreg(reg_name)
+  local reg_type = vim.fn.getregtype(reg_name)
+  vim.cmd(string.format('silent normal! "%sy', reg_name))
+  local text = vim.fn.getreg(reg_name)
+  vim.fn.setreg(reg_name, reg_value, reg_type)
+  return sanitize_search_text(text)
+end
+
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
@@ -34,58 +55,65 @@ return {
 
     vim.list_extend(opts.servers["*"].keys, {
       {
-        "<C-2>s",
+        "<leader>cs",
         snacks_only(function(picker)
-          picker.lsp_workspace_symbols(ivy_opts({
-            search = vim.fn.expand("<cword>"),
+          local mode = vim.api.nvim_get_mode().mode
+          local search = sanitize_search_text(vim.fn.expand("<cword>"))
+          if mode == "v" or mode == "V" or mode == "\22" then
+            search = get_visual_selection_text_by_yank()
+          end
+          picker.grep(ivy_opts({
+            search = search,
+            regex = false,
           }))
         end),
-        desc = "LSP Workspace Symbols",
+        mode = { "n", "x" },
+        desc = "Grep (cword/selection)",
       },
       {
-        "<C-2>g",
+        "<leader>cg",
         snacks_only(function(picker)
           picker.lsp_definitions(ivy_opts())
         end),
         desc = "LSP Definition",
       },
       {
-        "<C-2>c",
+        "<leader>cc",
         snacks_only(function(picker)
           picker.lsp_incoming_calls(ivy_opts())
         end),
         desc = "LSP Incoming Calls",
       },
       {
-        "<C-2>t",
+        "<leader>ct",
         snacks_only(function(picker)
           picker.lsp_references(ivy_opts())
         end),
         desc = "LSP References",
       },
       {
-        "<C-2>d",
+        "<leader>cd",
         snacks_only(function(picker)
           picker.lsp_outgoing_calls(ivy_opts())
         end),
         desc = "LSP Outgoing Calls",
       },
       {
-        "<C-2>e",
+        "<leader>ce",
         snacks_only(function(picker)
           picker.grep(ivy_opts())
         end),
         desc = "Live Grep",
       },
       {
-        "<C-2>f",
+        "<leader>cf",
         snacks_only(function(picker)
           picker.files(ivy_opts())
         end),
         desc = "Find Files",
       },
       {
-        "<C-2>i",
+        "<leader>ci",
         snacks_only(function(picker)
           picker.grep(ivy_opts({
             search = vim.fn.expand("<cfile>"),
